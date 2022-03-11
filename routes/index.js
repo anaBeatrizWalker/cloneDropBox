5b01f101527b573e891c2ad65540359a53ae6923
Ana Beatriz <anabiawalker@gmail.com>
2022-03-11 16:02:19 -0300

    Adicionando rota post e recebimento de arquivos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload',(req, res)=> {
+  let form = new formidable.IncomingForm({ //recupera o formulário
+    uploadDir: './upload', //diretório
+    keepExtensions: true //mantém a extensão dos arquivos
+  })
+  form.parse(req, (err, fields, files)=>{ //interpreta os dados = campos e arquivos
+    
+    res.json({ files })
+  })
+})
 
 module.exports = router;
